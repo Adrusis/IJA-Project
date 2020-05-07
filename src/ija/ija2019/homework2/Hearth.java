51c7a2709a8b17081bda5e798b0d8ed85eb4ab61
Martin Mikus <xmikus16@stud.fit.vutbr.cz>
2020-05-07 23:30:44 +0200

    Vylepsiene vnutorne hodiny + vytvorienie funkcie move

--- src/ija/ija2019/homework2/Hearth.java
+++ src/ija/ija2019/homework2/Hearth.java
@@ -1,12 +1,8 @@
 package ija.ija2019.homework2;
 
-import ija.ija2019.homework2.MyMaps.MyLine;
 import ija.ija2019.homework2.MyMaps.MySpoj;
-import ija.ija2019.homework2.MyMaps.MyStop;
 import ija.ija2019.homework2.maps.Line;
 import ija.ija2019.homework2.maps.Spoj;
-import ija.ija2019.homework2.maps.Stop;
-import javafx.scene.effect.Light;
 
 import java.io.*;
 import java.util.ArrayList;
@@ -14,42 +10,32 @@ import java.util.List;
 
 public class Hearth {
     private static List<Line> lines = new ArrayList<Line>();
-    private static String time;
 
-    public static void clock(){
-        int seconds = 0;
-        int minutes = -1;
-        int hours = 0;
+    public static void move(){
+        long time = 0;
+        String clock;
         while (true){
-            long runtime = System.nanoTime();
-            if((runtime%(1000000)) == 0){
-                seconds++;
-                if((seconds%60) == 0) {
-                    minutes++;
-                    if (hours < 10) {
-                        if (minutes < 10) {
-                            time = "0" + Integer.toString(hours) + ":" + "0" + Integer.toString(minutes);
-                        } else {
-                            time = "0" + Integer.toString(hours) + ":" + Integer.toString(minutes);
-                        }
-                    } else {
-                        if (minutes < 10) {
-                            time = Integer.toString(hours) + ":" + "0" + Integer.toString(minutes);
-                        } else {
-                            time = Integer.toString(hours) + ":" + Integer.toString(minutes);
-                        }
-                    }
-                    if (((minutes + 1) % 60) == 0) {
-                        hours++;
-                        minutes = 0;
-                        seconds = 0;
-                        if (hours == 24) {
-                            hours = 0;
-                        }
-                    }
-                }
-            }
+            time++;
+            clock = getTime(60,time);
         }
+    }
+    // za jednu sec 285991
+   public static String getTime (int modifier, long time){
+        int base = 285991;
+        long hour = base*(3600/modifier);
+        long min = base*(60/modifier);
+        long hours = (time/hour)%24;
+        long minutes = (time/min)%60;
+        if (hours < 10){
+            if (minutes < 10){
+                return "0"+Long.toString(hours)+":"+"0"+Long.toString(minutes);
+            }
+            return "0"+Long.toString(hours)+":"+Long.toString(minutes);
+        }
+        if (minutes < 10){
+            return "0"+Long.toString(hours)+":"+"0"+Long.toString(minutes);
+        }
+       return Long.toString(hours)+":"+Long.toString(minutes);
     }
 
     public static void setup() throws IOException {
@@ -82,7 +68,7 @@ public class Hearth {
 
     public static void main(String[] argc) throws IOException {
         setup();
-        clock();
+        move();
     }
 
 }
